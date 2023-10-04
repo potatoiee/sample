6214e8bd9d256cb3a111a0586494b7b219e5f910
potatoiee <markgeraldala813@gmail.com>
2023-10-05 02:45:47 +0800

    sample

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function UserDashboard(){
+
+        return view('user.user_dashboard');
+
+    }//end of the method
+}
